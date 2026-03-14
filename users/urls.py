c52b783d6a722d5cc1257e24364f50bef0dd4f52
dilafruz <dilafruz@gmail.com>
2026-03-14 12:12:18 +0500

    jwt_project

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import SignUpView, CodeVerifyView, GetNewCodeView, UserChangeInfoView, UserChangePhotoView, LoginView, \
-    LogoutView, LoginRefreshView
+    LogoutView, LoginRefreshView, ForgotPasswordView, ResetPasswordView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view()),
@@ -11,4 +11,6 @@ urlpatterns = [
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
     path('login-refresh/', LoginRefreshView.as_view()),
+    path('forgot-password/', ForgotPasswordView.as_view()),
+    path('reset-password/', ResetPasswordView.as_view()),
 ]
